263cf74bcf59f8b0fbe380828132644ebd11f1a8
HamGuy <wangrui15@gmail.com>
2020-06-24 22:13:15 +0800

    Initial Commit

---
+++ Shared/HGSwiftUIDempApp.swift
@@ -0,0 +1,17 @@
+//
+//  HGSwiftUIDempApp.swift
+//  Shared
+//
+//  Created by Miro on 2020/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HGSwiftUIDempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
