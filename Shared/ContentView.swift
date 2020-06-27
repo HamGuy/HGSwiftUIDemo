909ed3553a72009a5b30731ec582cf920b6ae279
HamGuy <wangrui15@gmail.com>
2020-06-27 11:17:33 +0800

    backyard offset animation

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,16 +14,18 @@ struct ContentView: View {
         self.course = CourseIntro(id: 0, name: "Section 1", briefIntro: "New Section", icon: "Logo", pic: Image("Illustration4"))
     }
     
-    @State var show = false
+    @State var showTitle = false
     @State var cardState = CGSize.zero
     @State var showCard = false
+    
+    let cardRounded = RoundedRectangle(cornerRadius: 16, style: .circular)
     
     var body: some View {
 //        NavigationView(content: {
             ZStack {
                 
                 CertificationHeaderView()
-                    .blur(radius: show ? 20.0 : 0.0)
+                    .blur(radius: showTitle ? 20.0 : 0.0)
                     .opacity(showCard ? 0.4 : 1)
                     .offset(x: 0, y: showCard ? -200 : 0)
                     .animation(
@@ -34,27 +36,33 @@ struct ContentView: View {
                 
                 
                 Rectangle()
-                    .fill( show ? Color.blue : Color.green)
-                    .frame(width: 320, height: 220, alignment: .center)
-                    .cornerRadius(16)
-                    .offset(x: 0, y: show ? -400 : -40)
-                    .scaleEffect(0.85).rotationEffect(.degrees( show ? 0 : 10))
+                    .fill( showTitle ? Color.blue : Color.green)
+                    .frame(width: showCard ? 280 : 320, height: 220, alignment: .center)
+                    .clipShape(cardRounded)
+                    .offset(x: 0, y: showTitle ? -400 : -40)
+                    .offset(x: 0 , y: showCard ? -180: -40)
+                    .scaleEffect(showCard ? 1 : 0.85)
+                    .rotationEffect(.degrees( showTitle || showCard ? 0 : 10))
                     .animation(.easeInOut(duration: 0.5))
                 
                 Rectangle()
-                    .fill( show ? Color.green : Color.blue)
+                    .fill( showTitle ? Color.green : Color.blue)
                     .frame(width: 320, height: 220, alignment: .center)
-                    .cornerRadius(16)
-                    .offset(x: 0, y: show ? -200 : -20)
-                    .scaleEffect(0.95).rotationEffect(.degrees(show ? 0 : 5))
+                    .clipShape(cardRounded)
+                    .offset(x: 0, y: showTitle ? -200 : -20)
+                    .offset(x: 0 , y: showCard ? -150: -20)
+                    .scaleEffect( showCard ? 1: 0.95)
+                    .rotationEffect(.degrees(showTitle || showCard ? 0 : 5))
                     .animation(.easeInOut(duration: 0.5))
                 
                 
                 CourseCardView(course: self.course)
                     .background(Color(hex: 0x11))
-                    .frame(width: 320, height: 220, alignment: .center)
-                    .cornerRadius(16)
+                    .frame(width: showCard ? UIScreen.main.bounds.width - 10 : 320, height: 220, alignment: .center)
+//                    .cornerRadius(16)
+                    .clipShape(cardRounded)
                     .offset(x: cardState.width, y: cardState.height)
+                    .offset(y: showCard ? -120 : 0)
                     .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                     .onTapGesture {
 //                        show.toggle()
@@ -64,15 +72,15 @@ struct ContentView: View {
                         DragGesture()
                             .onChanged{ value in
                                 cardState = value.translation
-                                show = true
+                                showTitle = true
                             }.onEnded { _ in
                                 cardState = .zero
-                                show = false
+                                showTitle = false
                             }
                     )
                 
                 CertificationBottomView()
-                    .blur(radius: show ? 20.0 : 0.0)
+                    .blur(radius: showTitle ? 20.0 : 0.0)
                     .offset(x: 0, y: showCard ? 360 : 1000)
                     .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.6))
 
